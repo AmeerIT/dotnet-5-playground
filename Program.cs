0049e3b46f57782b1bbc97c76f6c324eb924e8c8
Ameer Adel <ameer.adel87@gmail.com>
2020-11-22 15:04:45 +0300

    initial upload
    top level call method fun

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine(
+    System.Threading.Tasks.Task.Factory.StartNew(() =>
+    {
+        var filename = "something.txt";
+        if (!System.IO.File.Exists(filename))
+            System.IO.File.Create(filename);
+
+        System.IO.File.WriteAllText(filename, "hello world!");
+        return "This line will write hello world to something.txt from inside a task inside a console write line, this is a one line code";
+    }).Result +
+    $"{System.Environment.NewLine} " +
+    $"{System.IO.File.ReadAllText("something.txt")}");
